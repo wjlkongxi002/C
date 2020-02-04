a74192b2fdfafeba9462468261b15d199d4e532c
wjlkongxi002 <wjlkongxi@163.com>
2020-02-04 17:09:39 +0800

    寒假练习

---
+++ 寒假作业练习/寒假作业练习/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	//	int i = 0, a = 0;
+	//	while (i<20)
+	//	{
+	//		for (;;)
+	//		{
+	//			if ((i % 10) == 0) break;
+	//			else i--;
+	//		} 
+	//			i+= 11; a += i;
+	//	}
+	//	printf("%d\n", a);
+	//	return 0;
+	//} 
+	/*
+	#define N 2
+	#define M N+1
+	#define NUM (M+1)*M/2
+	int main()
+	{
+	printf("%d\n", NUM);
+	}
+	*/
+	int a = 1;
+	int func(int a);
+	{
+		int b;
+		switch (a)
+		{
+		case 1: b = 30;
+		case 2: b = 20;
+		case 3: b = 16;
+		default: b = 0;
+		}
+		return b;
+		
+	}
+}
