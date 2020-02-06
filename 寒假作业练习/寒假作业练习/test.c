507b53b49e5d0370185a2cb3254afa135be3dbc5
wjlkongxi002 <wjlkongxi@163.com>
2020-02-06 12:33:33 +0800

    更新

--- 寒假作业练习/寒假作业练习/test.c
+++ 寒假作业练习/寒假作业练习/test.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int main()
-{
+//int main()
+//{
 	//	int i = 0, a = 0;
 	//	while (i<20)
 	//	{
@@ -24,7 +24,7 @@ int main()
 	printf("%d\n", NUM);
 	}
 	*/
-	int a = 1;
+	/*int a = 1;
 	int func(int a);
 	{
 		int b;
@@ -36,6 +36,44 @@ int main()
 		default: b = 0;
 		}
 		return b;
-		
+	}*/
+	/*int m = 'ab';
+	int n = '\101';
+	int o = "A";
+	int p = PI;
+
+	printf("delete file (are you really sure??): ");*/
+/*
+			char *p = "abcdefgh", *r;
+			long *q;
+			q = (long*)p;
+			q++;
+			r = (char*)q;
+			printf("%s\n", r);
+		*/
+
+	
+			/*char str[] = "ABCD", *p = str;
+			printf("%d\n", *(p + 4));
+			return 0;*/
+	/*int f(int n);
+	int n=4;
+	{
+		if (n)
+			return f(n - 1) + n;
+		else
+			return n;
+	}*/
+	//printf("\a\a\a\a\a");
+//}
+	int Add(int a, int b)
+	{
+		int c = 0;
+		c = a + b;
+		return c;
 	}
-}
+	int main()
+	{
+		//Add(10,20);
+		printf("%d", Add(10, 20));
+	}
